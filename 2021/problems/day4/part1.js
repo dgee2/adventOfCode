b62bc11ad8bd20c488406772713954e801ca3bf8
Daniel Gee <dan@dan-gee.co.uk>
2021-12-06 00:28:52 +0000

    Add logic to parse boards

--- 2021/problems/day4/part1.js
+++ 2021/problems/day4/part1.js
@@ -1,4 +1,27 @@
 const { loadPuzzleInput } = require("../../helpers");
+const { EOL } = require("os")
 
-const puzzleInput = loadPuzzleInput(__dirname)
+const { boardOptions, bingoNumbers } = loadBoardAndNumbers();
+console.log(boardOptions)
+console.log(bingoNumbers)
 
+function rotateMatrix(matrix) {
+    return matrix[0].map((val, index) => matrix.map(row => row[index]).reverse());
+}
+function loadBoardAndNumbers() {
+    const puzzleInput = loadPuzzleInput(__dirname).replaceAll("  ", " ").split(EOL + EOL);
+
+    const numbers = parseBingoNumbers(puzzleInput.shift());
+
+    const boards = puzzleInput.map(parseBoard)
+    return { bingoNumbers: numbers, boardOptions: boards }
+}
+function parseBoard(input) {
+    const rows = input
+        .split(EOL)
+        .map((r) => r.trim().split(" ").map(Number));
+    return rotateMatrix(rows).concat(rows)
+}
+function parseBingoNumbers(input) {
+    return input.split(",").map(Number)
+}
