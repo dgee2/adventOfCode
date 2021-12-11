acfdddf28d027a5aa2a28acc1b4020ae9015bedc
Daniel Gee <dan@dan-gee.co.uk>
2021-12-11 09:36:07 +0000

    day 6 part 2

---
+++ 2021/problems/day6/part2.js
@@ -0,0 +1,21 @@
+const { loadPuzzleInput } = require('../../helpers');
+let puzzleInput = loadPuzzleInput(__dirname).split(',').map(Number);
+
+console.log(puzzleInput)
+/**
+ * 
+ * @param {Number[]} puzzleInput 
+ */
+function incrementDay(input) {
+    return input.flatMap(x => {
+        if (x === 0) {
+            return [6, 8]
+        } else {
+            return x - 1
+        }
+    })
+}
+for (let i = 0; i < 256; i++) {
+    puzzleInput = incrementDay(puzzleInput)
+}
+console.log(puzzleInput.length)
